67454724bcbf24d03afaebe4bd4dbd0bd0a4ebd0
ButterDebugger <34288129+ButterDebugger@users.noreply.github.com>
2025-12-08 18:37:38 -0500

    Updated test cases

--- test/quadtree.test.ts
+++ test/quadtree.test.ts
@@ -1,25 +1,44 @@
-import { assertEquals } from "@std/assert";
+import { assertArrayIncludes, assertEquals } from "@std/assert";
 import { QuadTree, Point, BoundingCircle, BoundingRectangle } from "../src/index.ts";
 
-Deno.test("quadtree", () => {
-  const outerBound = new BoundingRectangle(0, 0, 100, 100);
-  const tree = new QuadTree<string>(outerBound, 4);
+const outerBound = new BoundingRectangle(0, 0, 100, 100);
+const tree = new QuadTree<string>(outerBound, 4);
 
-  // Insert some points
-  const p1 = new Point(10, 10, "p1");
-  const p2 = new Point(20, 20, "p2");
-  const p3 = new Point(30, 30, "p3");
-  const p4 = new Point(40, 40, "p4");
-  const p5 = new Point(50, 50, "p5");
+// Insert some points
+//     4
+// 2 3
+// 1   5
+const p1 = new Point(25, 25, "bottom-left");
+const p2 = new Point(25, 50, "middle-left");
+const p3 = new Point(50, 50, "center");
+const p4 = new Point(75, 75, "top-right");
+const p5 = new Point(75, 25, "bottom-right");
 
-  tree.insert(p1);
-  tree.insert(p2);
-  tree.insert(p3);
-  tree.insert(p4);
-  tree.insert(p5);
+tree.insert(p1);
+tree.insert(p2);
+tree.insert(p3);
+tree.insert(p4);
+tree.insert(p5);
 
-  // Query the entire tree
+Deno.test("Query entire tree", () => {
   const points = tree.query(outerBound);
 
+  assertArrayIncludes(points, [p1, p2, p3, p4, p5]);
   assertEquals(points.length, 5);
+});
+
+Deno.test("Query rectangle", () => {
+  const rect = new BoundingRectangle(0, 0, 50, 50);
+  const points = tree.query(rect);
+
+  assertArrayIncludes(points, [p1, p2, p3]);
+  assertEquals(points.length, 3);
+});
+
+Deno.test("Query circle", () => {
+  const circle = new BoundingCircle(50, 50, 30);
+  const points = tree.query(circle);
+
+  assertArrayIncludes(points, [p2, p3]);
+  assertEquals(points.length, 2);
 });
